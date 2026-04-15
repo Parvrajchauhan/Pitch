09575969bd8819ad3af0988e4bb1051826b998be
Parvrajchauhan <parvrajchauhan@gmail.com>
2026-04-16 01:55:30 +0530

    Base Frontend completed

--- server/api/main.py
+++ server/api/main.py
@@ -1,8 +1,21 @@
-# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.route.routes import router
 
 app = FastAPI(title="Pitch", version="1.0.0")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
